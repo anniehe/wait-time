a322911910581c1e7e07b9b7e554f20b1c1937a3
Annie He <anniehe.yt@gmail.com>
2016-02-08 14:48:47 -0800

    Yelp calls using yelpapi by Geoffrey Fairchild works

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request
+from yelpapi import YelpAPI
+import os
+import pprint
 
 
 app = Flask(__name__)
@@ -16,6 +19,35 @@ def display_search_results():
     """Display search results."""
 
     return render_template("results.html")
+
+
+##################
+#### YELP API ####
+
+consumer_key = os.environ['YELP_CONSUMER_KEY']
+consumer_secret = os.environ['YELP_CONSUMER_SECRET']
+token = os.environ['YELP_TOKEN']
+token_secret = os.environ['YELP_TOKEN_SECRET']
+
+yelp_api = YelpAPI(consumer_key, consumer_secret, token, token_secret)
+search_results = yelp_api.search_query(term='Sanraku', location='san francisco, ca', limit=5)
+
+# pprint.pprint(search_results)
+
+name = search_results['businesses'][0]['name']
+print "NAME", name
+business_id = search_results['businesses'][0]['id']
+print "BUSINESS ID", business_id
+review_count = search_results['businesses'][0]['review_count']
+print "REVIEW COUNT", review_count
+rating = search_results['businesses'][0]['rating']
+print "RATING", rating
+
+# business_results = yelp_api.business_query(id=business_id)
+# print "BUSINESS RESULTS\n"
+# pprint.pprint(business_results)
+
+
 
 
 if __name__ == "__main__":
