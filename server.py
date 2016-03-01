c7c011f5c9ec6edfd09f6f917afcf373fe9b878c
Annie He <anniehe.yt@gmail.com>
2016-02-29 17:31:24 -0800

    Working on testing add_open_status, separated out helper functions from server

--- server.py
+++ server.py
@@ -3,13 +3,13 @@ from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
 
 from model import WaitTime, connect_to_db, db
+from process_results import add_open_status, add_wait_info, sorted_result, filtered_result
 
 from yelp_api import yelp
-from google_api import is_open_now, BROWSER_KEY
+from google_api import BROWSER_KEY
 from twilio_api import send_thank_you_sms, send_reminder_sms, convert_to_e164
 
 from datetime import datetime
-import arrow
 import threading
 
 
@@ -170,142 +170,6 @@ def process_wait_time_form():
     flash("Thanks for reporting your wait time!")
 
     return redirect("/")
-
-
-### HELPER FUNCTIONS ###
-
-def add_open_status(restaurant):
-    """Add restaurant's open status at the current time to the its dictionary."""
-
-    # Find matching Google Places open status info for the Yelp restaurant result
-    name = restaurant['name']
-    address = restaurant['location']['address'][0]
-    city = restaurant['location']['city']
-    location_lat = restaurant['location']['coordinate']['latitude']
-    location_lng = restaurant['location']['coordinate']['longitude']
-
-    keyword = "%s %s %s" % (name, address, city)
-    location = "%f,%f" % (location_lat, location_lng)
-
-    open_now = is_open_now(keyword, location)
-    # open_now is True, False, or None
-    if open_now:
-        open_now = "Open now"
-    elif open_now is None:
-        open_now = "Open now unknown"
-    else:
-        open_now = "Closed"
-
-    # Add open now info to dictionary
-    restaurant['open_now'] = open_now
-
-
-def add_wait_info(restaurant):
-    """Add restaurant's wait time information from database to its dictionary."""
-
-    yelp_id = restaurant['id']
-
-    # Find the most recent wait time info for a restaurant from the database.
-    # For records with the same timestamp, fetch the largest quoted wait time.
-    wait_info = (WaitTime.query.filter_by(yelp_id=yelp_id)
-                 .order_by(WaitTime.timestamp.desc(), WaitTime.quoted_minutes.desc())
-                 .first()
-                 )
-
-    # Add wait time info to dictionary
-    if wait_info:
-        restaurant['quoted_wait_time'] = wait_info.quoted_minutes
-        restaurant['timestamp'] = arrow.get(wait_info.timestamp).humanize()
-        restaurant['timestamp_value'] = wait_info.timestamp
-
-        if wait_info.party_size:
-            restaurant['party_size'] = wait_info.party_size
-        else:
-            restaurant['party_size'] = "N/A"
-
-        if wait_info.parties_ahead:
-            restaurant['parties_ahead'] = wait_info.parties_ahead
-        else:
-            restaurant['parties_ahead'] = "N/A"
-
-    else:
-        restaurant['quoted_wait_time'] = "Not available"
-        restaurant['party_size'] = "N/A"
-        restaurant['parties_ahead'] = "N/A"
-        restaurant['timestamp'] = "N/A"
-        restaurant['timestamp_value'] = datetime(2000, 2, 2)
-
-
-def sorted_result(result, sort_value):
-    """Returns result sorted by the selected value."""
-
-    # Sort by recently reported
-    if sort_value == "recently_reported":
-        result.sort(key=lambda restaurant: restaurant['timestamp_value'], reverse=True)
-
-    # Sort by shortest wait time
-    if sort_value == "wait_time":
-        result.sort(key=lambda restaurant: restaurant['quoted_wait_time'])
-
-    # Sort by highest rating
-    if sort_value == "rating":
-        result.sort(key=lambda restaurant: restaurant['rating'], reverse=True)
-
-    # Sort by most reviews
-    if sort_value == "review_count":
-        result.sort(key=lambda restaurant: restaurant['review_count'], reverse=True)
-
-    return result
-
-
-def filtered_result(result, selected_filters):
-    """Returns filtered result."""
-
-    # Filter by open now
-    if "open_now" in selected_filters:
-        new_result = []
-        for restaurant in result:
-            if restaurant['open_now'] == "Open now":
-                new_result.append(restaurant)
-        result = new_result
-
-    # Filter by <=15 min wait
-    if "15_min_wait" in selected_filters:
-        new_result = []
-        for restaurant in result:
-            if (restaurant['quoted_wait_time'] != "Not available" and
-                    restaurant['quoted_wait_time'] <= 15):
-                new_result.append(restaurant)
-        result = new_result
-
-    # Filter by <=30 min wait
-    if "30_min_wait" in selected_filters:
-        new_result = []
-        for restaurant in result:
-            if (restaurant['quoted_wait_time'] != "Not available" and
-                    restaurant['quoted_wait_time'] <= 30):
-                new_result.append(restaurant)
-        result = new_result
-
-    # Filter by <=45 min wait
-    if "45_min_wait" in selected_filters:
-        new_result = []
-        for restaurant in result:
-            if (restaurant['quoted_wait_time'] != "Not available" and
-                    restaurant['quoted_wait_time'] <= 45):
-                new_result.append(restaurant)
-        result = new_result
-
-    # Filter by <=60 min wait
-    if "60_min_wait" in selected_filters:
-        new_result = []
-        for restaurant in result:
-            if (restaurant['quoted_wait_time'] != "Not available" and
-                    restaurant['quoted_wait_time'] <= 60):
-                new_result.append(restaurant)
-        result = new_result
-
-    return result
 
 
 if __name__ == "__main__":
--- tests.py
+++ tests.py
@@ -1,7 +1,10 @@
 import unittest
 import doctest
 import server
+from server import app, sorted_result, filtered_result, add_open_status
+from unittest import TestCase
 from twilio_api import convert_to_e164, send_thank_you_sms, send_reminder_sms
+from google_api import is_open_now
 
 # To test:
 # python tests.py
@@ -9,6 +12,36 @@ from twilio_api import convert_to_e164, send_thank_you_sms, send_reminder_sms
 # Coverage and Report:
 # coverage run --source=. --omit=env* tests.py
 # coverage report -m
+
+
+
+
+restaurant_mock = {
+                    "rating": 4.0,
+                    "rating_img_url": "https://s3-media4.fl.yelpcdn.com/assets/2/www/img/c2f3dd9799a5/ico/stars/v1/stars_4.png",
+                    "review_count": 1983,
+                    "name": "Ryoko's",
+                    "rating_img_url_small": "https://s3-media4.fl.yelpcdn.com/assets/2/www/img/f62a5be2f902/ico/stars/v1/stars_small_4.png",
+                    "url": "http://www.yelp.com/biz/ryokos-san-francisco?utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=6XuCRI2pZ5pIvcWc9SI3Yg",
+                    "image_url": "https://s3-media4.fl.yelpcdn.com/bphoto/El1KekyVFSqrtKLAyjcfNA/ms.jpg",
+                    "display_phone": "+1-415-775-1028",
+                    "id": "ryokos-san-francisco",
+                    "location": {
+                        "city": "San Francisco",
+                        "postal_code": "94102",
+                        "address": [
+                            "619 Taylor St"
+                        ],
+                        "coordinate": {
+                            "latitude": 37.788008004427,
+                            "longitude": -122.411782890558
+                        },
+                        "state_code": "CA"
+                    }
+                }
+
+
+
 
 
 def load_tests(loader, tests, ignore):
@@ -19,7 +52,7 @@ def load_tests(loader, tests, ignore):
     return tests
 
 
-class TwilioUnitTestCase(unittest.TestCase):
+class TwilioUnitTestCase(TestCase):
     """Unit tests on Twilio."""
 
     def test_convert_to_e164(self):
@@ -53,37 +86,127 @@ class TwilioUnitTestCase(unittest.TestCase):
         self.assertEqual(send_reminder_sms("+1231231223", "Sanraku", "15 min"), "Error")
 
 
-class MyAppIntegrationTestCase(unittest.TestCase):
+class MockFlaskTests(TestCase):
+    """Mock flask tests for Google and Yelp."""
 
     def setUp(self):
         """Do before every test."""
 
-        self.client = server.app.test_client()
+        # Get the Flask test client
+        self.client = app.test_client()
 
-        def _mock_add_open_status(restaurant):
-            pass
+        # Show Flask errors that happen during tests
+        app.config['TESTING'] = True
 
-        def _mock_add_wait_info(restaurant):
-            pass
+        # Make mocks
 
-        def _mock_sorted_result(result, sort_value):
-            pass
+        restaurant_mock = {
+                            "rating": 4.0,
+                            "rating_img_url": "https://s3-media4.fl.yelpcdn.com/assets/2/www/img/c2f3dd9799a5/ico/stars/v1/stars_4.png",
+                            "review_count": 1983,
+                            "name": "Ryoko's",
+                            "rating_img_url_small": "https://s3-media4.fl.yelpcdn.com/assets/2/www/img/f62a5be2f902/ico/stars/v1/stars_small_4.png",
+                            "url": "http://www.yelp.com/biz/ryokos-san-francisco?utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=6XuCRI2pZ5pIvcWc9SI3Yg",
+                            "image_url": "https://s3-media4.fl.yelpcdn.com/bphoto/El1KekyVFSqrtKLAyjcfNA/ms.jpg",
+                            "display_phone": "+1-415-775-1028",
+                            "id": "ryokos-san-francisco",
+                            "location": {
+                                "city": "San Francisco",
+                                "postal_code": "94102",
+                                "address": [
+                                    "619 Taylor St"
+                                ],
+                                "coordinate": {
+                                    "latitude": 37.788008004427,
+                                    "longitude": -122.411782890558
+                                },
+                                "state_code": "CA"
+                            }
+                        }
 
-        def _mock_filtered_result(result, selected_filters):
-            pass
+        def _mock_get_yelp_search_results(term, location, category_filter, limit):
+            search_results = {
+                "businesses": [
+                    {
+                        "rating": 4.0,
+                        "rating_img_url": "https://s3-media4.fl.yelpcdn.com/assets/2/www/img/c2f3dd9799a5/ico/stars/v1/stars_4.png",
+                        "review_count": 1983,
+                        "name": "Ryoko's",
+                        "rating_img_url_small": "https://s3-media4.fl.yelpcdn.com/assets/2/www/img/f62a5be2f902/ico/stars/v1/stars_small_4.png",
+                        "url": "http://www.yelp.com/biz/ryokos-san-francisco?utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=6XuCRI2pZ5pIvcWc9SI3Yg",
+                        "image_url": "https://s3-media4.fl.yelpcdn.com/bphoto/El1KekyVFSqrtKLAyjcfNA/ms.jpg",
+                        "display_phone": "+1-415-775-1028",
+                        "id": "ryokos-san-francisco",
+                        "location": {
+                            "city": "San Francisco",
+                            "postal_code": "94102",
+                            "address": [
+                                "619 Taylor St"
+                            ],
+                            "coordinate": {
+                                "latitude": 37.788008004427,
+                                "longitude": -122.411782890558
+                            },
+                            "state_code": "CA"
+                        }
+                    },
+                    {
+                        "rating": 4.5,
+                        "rating_img_url": "https://s3-media2.fl.yelpcdn.com/assets/2/www/img/99493c12711e/ico/stars/v1/stars_4_half.png",
+                        "review_count": 547,
+                        "name": "Saru Sushi Bar",
+                        "rating_img_url_small": "https://s3-media2.fl.yelpcdn.com/assets/2/www/img/a5221e66bc70/ico/stars/v1/stars_small_4_half.png",
+                        "url": "http://www.yelp.com/biz/saru-sushi-bar-san-francisco?utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=6XuCRI2pZ5pIvcWc9SI3Yg",
+                        "image_url": "https://s3-media2.fl.yelpcdn.com/bphoto/5-ugy01zjSvudVsfdhmCsA/ms.jpg",
+                        "display_phone": "+1-415-400-4510",
+                        "id": "saru-sushi-bar-san-francisco",
+                        "location": {
+                            "city": "San Francisco",
+                            "postal_code": "94114",
+                            "country_code": "US",
+                            "address": [
+                                "3856 24th St"
+                            ],
+                            "coordinate": {
+                                "latitude": 37.751706,
+                                "longitude": -122.4288283
+                            },
+                            "state_code": "CA"
+                        }
+                    }
+                ]
+            }
+
+            return search_results
 
         def _mock_is_open_now(keyword, location):
-            pass
+            return True
 
-        def _mock_get_yelp_search_results():
-            pass
+        self.old_get_yelp_search_results = server.yelp.search_query
+        server.yelp.search_query = _mock_get_yelp_search_results
 
-        self._old_add_open_status = server.add_open_status
-        server.add_open_status = _mock_add_open_status
+        self.old_is_open_now = server.is_open_now
+        server.is_open_now = _mock_is_open_now
 
     def tearDown(self):
         """Do at the end of every test."""
-        server.add_open_status = self._old_state_to_code
+
+        server.yelp.search_query = self.old_get_yelp_search_results
+        server.is_open_now = self.old_is_open_now
+
+    def test_add_open_status_with_mock(self):
+        add_open_status(restaurant_mock)
+        self.assertEqual(restaurant_mock["open_now"], "Open now")
+
+    def test_add_wait_info_with_mock(restaurant):
+        pass
+
+    # def test_sorted_result_with_mock(result, sort_value):
+    #     result = _mock_get_yelp_search_results["businesses"]
+    #     self.assertEqual(sorted_result()
+
+    # def test_filtered_result_with_mock(result, selected_filters):
+    #     pass
 
 
 if __name__ == '__main__':
